1e504d8c8b9d5e211bd183c793bfec177271f046
Changwan Jun <im@wani.kr>
2018-06-16 05:06:13 +0900

    complete header intro

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,64 @@
 <template>
-  <section class="container">
+  <div>
 
-    <template>
-      <div class="header en">
-        
+    <div class="section section-header en">
+      <div class="container">
         <div class="logo"></div>
 
-        <div class="label-top">
-          Until next DODO
-        </div>
+        <a class="button button-text">참가신청</a>
+        <a class="button button-icon">??</a>
+      </div>
+    </div>
 
-        <div class="d-day-container">
+    <div class="section section-intro">
+      <div class="container">
+        <div class="title en">Until next DODO</div>
+        <div class="d-day-container en">
           <div class="d-day" v-if="dday !== null">D-{{ dday }}</div>
           <div class="d-day" v-else-if="dday === 0">D-{{ dday }}</div>
           <div class="d-day" v-else>지.남</div>
         </div>
-
-        <div class="title">
-          DO <span class="span">What you want to</span> DO
+        <div class="description en">DO <span class="span">What you want to</span> DO</div>
+        <div class="buttons">
+          <a class="button button-slack en" href="https://www.slack.com" target="blank">Slack</a>
+          <a class="button button-gallery en" href="??" target="blank">Gallery</a>
+          <a class="button button-github en" href="https://github.com/we-are-dodo" target="blank">Github</a>
         </div>
-
-        <!-- 미디어 링크들 -->
-        <div class="media-container">
-          <a href="https://www.slack.com" target="blank">
-            <div class="box slack">
-              <div class="text">Slack</div>
-            </div>
-          </a>
-
-          <a href="https://www.slack.com" target="blank">
-            <div class="box facebook">
-              <div class="text">Group</div>
-            </div>
-          </a>
-
-          <a href="https://github.com/we-are-dodo" target="blank">
-            <div class="box github">
-              <div class="text">Github</div>
-            </div>
-          </a>
-        </div>
-        <!--  -->
-
       </div>
-    </template>
-
-    <!--  -->
+    </div>
 
     <div class="history en">
         
-        <div class="subject" v-for="meet in meets" :key="meet.date">
+      <div class="subject" v-for="meet in meets" :key="meet.date">
 
-          <div class="event-title">
-            {{ meet.date }}
-          </div>
-
-          <div class="user-work-list">
-
-            <div class="content">
-              <div v-html="meet.contents"></div>
-            </div>
-
-            <div v-for="user in meet.users" :key="user.id">
-              <div class="user-name">
-                {{ user.name }}
-              </div>
-              
-              <div v-for="work in user.works" :key="work">
-                <div class="user-work">
-                  {{ work }}
-                </div>
-              </div>
-            </div>
-
-          </div>
-          
+        <div class="event-title">
+          {{ meet.date }}
         </div>
 
-    </div>
+        <div class="user-work-list">
 
-  </section>
+          <div class="content">
+            <div v-html="meet.contents"></div>
+          </div>
+
+          <div v-for="user in meet.users" :key="user.id">
+            <div class="user-name">
+              {{ user.name }}
+            </div>
+            
+            <div v-for="work in user.works" :key="work">
+              <div class="user-work">
+                {{ work }}
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 
 <script>
 
